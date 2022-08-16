e3856baac07cd08858c0b861a64404372cb77d7c
Myroslav Tovtyk <tovmyr@gmail.com>
2022-08-16 15:05:51 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
+const bodyEl = document.querySelector("body");
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
+
+btnStartEl.addEventListener('click', onStartBtnClick);
+btnStopEl.addEventListener('click', onStopBtnClick);
+
+let timerInterval = null;
+
+btnStopEl.setAttribute('disabled', true);
+
+function onStartBtnClick(e) {
+    if (e.target) {  
+        btnStartEl.setAttribute('disabled', true);
+        btnStopEl.removeAttribute('disabled');
+        timerInterval = setInterval(() => {            
+            return bodyEl.style.backgroundColor = getRandomHexColor();
+        }, 1000);        
+    };
+}
+
+function onStopBtnClick(e) {
+    if (e.target) {
+        btnStartEl.removeAttribute('disabled');
+        btnStopEl.setAttribute('disabled', true);
+        clearInterval(timerInterval);
+    };    
+};
