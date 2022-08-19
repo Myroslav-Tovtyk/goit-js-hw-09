b5425de5e1fb68526c36eb665378425913e74f65
Myroslav Tovtyk <tovmyr@gmail.com>
2022-08-19 12:36:37 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const refs = {    
+  btnSubmit: document.querySelector('.form'),
+};
+
+refs.btnSubmit.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const formData = new FormData(refs.btnSubmit);  
+  const delay = formData.get("delay");
+  const step = formData.get("step");
+  const amount = formData.get("amount");
+  generatorPromices(delay, step, amount);
+  e.target.reset();
+};
+
+function generatorPromices(delay, step, amount) {
+  for (let i = 0; i < amount; i++){
+    delay += step;
+
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      })
   }
 }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      };
+    }, delay);
+  });
+};
